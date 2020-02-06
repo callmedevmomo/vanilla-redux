3e000c58be17a4ff6db1f7ad89b780eb68e96b91
momo <callmedevmomo@gmail.com>
2020-02-06 14:37:29 +0900

    basic add/minus btn event handling with vanilla JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const plus = document.getElementById("Add");
+const minus = document.getElementById("Minus");
+const number = document.querySelector("span");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let count = 0;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count += 1;
+  updateText();
+};
+const handleMinus = () => {
+  count -= 1;
+  updateText();
+};
+
+plus.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
