b1cee446644783c29927c30c15c34563be48e9e1
momo <callmedevmomo@gmail.com>
2020-02-07 17:09:11 +0900

    set up with hooks

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,26 @@
-export default () => "Home";
+import React, { useState } from "react";
+
+function Home() {
+  // use Hooks
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange}></input>
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
